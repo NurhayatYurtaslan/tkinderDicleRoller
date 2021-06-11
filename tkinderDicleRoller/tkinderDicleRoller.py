929e181411c7f865cc4eb226c90205e7dc5fd6c0
nurhayat yurtaslan <nurhayatyurtaslan38@gmail.com>
2021-06-11 18:16:13 +0300

    tkinderDicleRoller

--- tkinderDicleRoller/tkinderDicleRoller.py
+++ tkinderDicleRoller/tkinderDicleRoller.py
@@ -21,7 +21,6 @@ c.create_image( 0, 0, image = bg, anchor = "nw")
 
 def roll_dice():
     global bttn_clicks
-    # unicode character strings for dice
     dice = ['\u2680', '\u2681', '\u2682', '\u2683', '\u2684', '\u2685']
     d = {'\u2680':1, '\u2681':2, '\u2682':3, '\u2683':4, '\u2684':5, '\u2685':6}
     die1 = random.choice(dice)
@@ -57,8 +56,5 @@ label1 = tk.Label(r, text='', font=('Girassol',20,'bold'),fg='black')
 c.create_window(180, 410, window=label1)
 label2 = tk.Label(r, text='Not rolled yet', font=('Girassol',20,'bold'),bg='purple',fg='white',width=12)
 c.create_window(480, 410, window=label2)
-#label3 = tk.Label(r, text='Winning rule: The player wins if he/she gets a sum of 10 on rolling 2 dice,within 10 chances', font=('Times',13,'bold'),fg='white',bg="black")
-#c.create_window(300, 450, window=label3)
 
-# call the mainloop of Tk
 r.mainloop()
